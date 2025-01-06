fc9f784602835162f2958a35d9cfe2ce16b6e871
kareem <kareem@kiebot.com>
2025-01-06 12:15:19 +0530

    add search change

--- src/app/search/[[...myParams]]/page.tsx
+++ src/app/search/[[...myParams]]/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function page(props: any) {
   console.log(props);
-  return <div>search page /hi/h2/h3</div>;
+  return <div>search page /hi/h2/h3/h4</div>;
 }
 
 export default page;
