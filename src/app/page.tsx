3c9526eae6bf78c00c56f4348813cd28ff2c3f82
kareem <kareem@kiebot.com>
2025-03-14 15:36:25 +0530

    add new changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Link from "next/link";
-import HomeButtonComponent, { HomeButton, HomeButton2, HomeButton3 } from "./Components/buttons/homeButton";
+import HomeButtonComponent, {
+  HomeButton,
+  HomeButton2,
+  HomeButton3,
+} from "./Components/buttons/homeButton";
 
 export default function Home() {
   return (
@@ -10,6 +14,7 @@ export default function Home() {
       <HomeButton>HomeStyledButton1</HomeButton>
       <HomeButton2>HomeStyledButton2</HomeButton2>
       <HomeButton3>HomeStyledButton3</HomeButton3>
+      <Image src="/next.svg" alt="shirt" width={200} height={200} />
       <HomeButtonComponent></HomeButtonComponent>
       <Link href={"product/#rc"}>
         <h3>Go To Recommended Products Section</h3>
--- src/app/search/[[...myParams]]/page.tsx
+++ src/app/search/[[...myParams]]/page.tsx
@@ -2,7 +2,14 @@ import React from "react";
 
 function page(props: any) {
   console.log(props);
-  return <div>search page /hi/h2/h3/h4</div>;
+  return (
+    <div>
+      localhost:3000/search/hi/h2/h3/h4
+      <h1>{props.params.myParams}</h1>
+      <h1>{props?.params?.myParams[0]}</h1>
+      <h1>{props?.params?.myParams[props?.params?.myParams?.length - 1]}</h1>
+    </div>
+  );
 }
 
 export default page;
