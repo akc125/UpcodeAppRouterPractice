0597839bb63ff0519eca53ed96c859076b36ea36
kareem <kareem@kiebot.com>
2025-03-17 10:53:25 +0530

    add dote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       <HomeButton>HomeStyledButton1</HomeButton>
       <HomeButton2>HomeStyledButton2</HomeButton2>
       <HomeButton3>HomeStyledButton3</HomeButton3>
-      <Image src="./next.svg" alt="shirt" width={200} height={200} />
+      <Image src="/next.svg" alt="shirt" width={200} height={200} />
       <HomeButtonComponent></HomeButtonComponent>
       <Link href={"product/#rc"}>
         <h3>Go To Recommended Products Section</h3>
